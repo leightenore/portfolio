6d9c74dc815f1db6155d93423be0a20618833761
ltenore <leightenore@gmail.com>
2019-10-02 12:54:15 -0700

    updated code with redesign

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,83 +1,118 @@
-//Click Events
-//===============================================================================================================
+$(document).ready(function () {
 
-$("#aboutMeLink").on("click", function(event) {
-    $("#portfolioCard").css("display","none");
-    $("#contactCard").css("display","none");
-    $("#bioCard").css("display","block");
+    // Scroll Top Button
+    if (window.scrollY == 0) {
+        document.getElementById("back2Top").style.visibility = "hidden";
+    }
+
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            document.getElementById("back2Top").style.visibility = "visible";
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
+
+    $(document).ready(function () {
+        $("#back2Top").click(function (event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, 500);
+            return false;
+        });
+
+    });
+
+    //Scroll to Skills Section On Click Function
+    $("#skillsLink").on("click", function (event) {
+        $([document.documentElement, document.body]).animate({ scrollTop: $("#skills-section").offset().top }, 500);
+    });
+
+    //Scroll to Portfolio Section On Click Function
+    $("#projectLink").on("click", function (event) {
+        $([document.documentElement, document.body]).animate({ scrollTop: $("#portfolio-section").offset().top }, 500);
+    });
+
+    //Scroll to Contact Section On Click Function
+    $("#contactLink").on("click", function (event) {
+        $([document.documentElement, document.body]).animate({ scrollTop: $("#contact-section").offset().top }, 500);
+    });
+
+    //CareConnect Modal Buttons
+    $("#careConnectApp").on("click", function (event) {
+        window.open("https://still-journey-26391.herokuapp.com/");
+    });
+
+    $("#careConnectCode").on("click", function (event) {
+        window.open("https://github.com/ethantatum/Project-Three");
+    });
+
+    //Off the Beaten Path Modal Buttons
+    $("#OTBPApp").on("click", function (event) {
+        window.open("https://offthebeatenpath.herokuapp.com/");
+    });
+
+    $("#OTBPCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/travel-app");
+    });
+
+    //ISS Seeker Modal Buttons
+    $("#ISSApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/iss-seeker/");
+    });
+
+    $("#ISSCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/iss-seeker");
+    });
+
+    //Game of Thrones Hangman Modal Buttons
+    $("#GotApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/got-hangman/");
+    });
+
+    $("#GotCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/got-hangman");
+    });
+
+    //Clicky Game Modal Buttons
+    $("#clickyApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/clicky-game/");
+    });
+
+    $("#clickyCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/clicky-game");
+    });
+
+    //LIRI Modal Button
+    $("#liriCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/liri-node-app");
+    });
+
+    //Giphy Modal Buttons
+    $("#giphyApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/Giphy-API/");
+    });
+
+    $("#giphyCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/Giphy-API");
+    });
+
+    //Trivia Modal Buttons
+    $("#triviaApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/TriviaGame/");
+    });
+
+    $("#triviaCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/TriviaGame");
+    });
+
+    //Crystal Modal Buttons
+    $("#crystalApp").on("click", function (event) {
+        window.open("https://leightenore.github.io/unit-4-game/");
+    });
+
+    $("#crystalCode").on("click", function (event) {
+        window.open("https://github.com/leightenore/unit-4-game");
+    });
 });
-
-$("#portfolioLink").on("click", function(event) {
-    $("#portfolioCard").css("display","block");
-    $("#contactCard").css("display","none");
-    $("#bioCard").css("display","none");
-});
-
-$("#contactLink").on("click", function(event) {
-    $("#portfolioCard").css("display","none");
-    $("#contactCard").css("display","block");
-    $("#bioCard").css("display","none");
-});
-
-$("#aboutMeLink1").on("click", function(event) {
-    $("#portfolioCard").css("display","none");
-    $("#contactCard").css("display","none");
-    $("#bioCard").css("display","block");
-});
-
-$("#portfolioLink1").on("click", function(event) {
-    $("#portfolioCard").css("display","block");
-    $("#contactCard").css("display","none");
-    $("#bioCard").css("display","none");
-});
-
-$("#contactLink1").on("click", function(event) {
-    $("#portfolioCard").css("display","none");
-    $("#contactCard").css("display","block");
-    $("#bioCard").css("display","none");
-});
-
-$("#connect1").on("click", function(event) {
-    var url = "https://www.linkedin.com/in/leigh-tenore-55a1b0172/";
-    window.open(url, '_blank');
-});
-
-$("#connect2").on("click", function(event) {
-    var url = "https://github.com/leightenore";
-    window.open(url, '_blank');
-});
-
-$("#project1img").on("click", function(event) {
-    var url = "https://leightenore.github.io/iss-seeker/";
-    window.open(url, '_blank');
-});
-
-$("#project2img").on("click", function(event) {
-    var url = "https://off-the-beaten-path.herokuapp.com/";
-    window.open(url, '_blank');
-});
-
-$("#hangmanimg").on("click", function(event) {
-    var url = "https://leightenore.github.io/got-hangman/";
-    window.open(url, '_blank');
-});
-
-$("#liriimg").on("click", function(event) {
-    var url = "https://github.com/leightenore/liri-node-app";
-    window.open(url, '_blank');
-});
-
-$("#giphyimg").on("click", function(event) {
-    var url = "https://leightenore.github.io/Giphy-API/";
-    window.open(url, '_blank');
-});
-
-$("#crystalimg").on("click", function(event) {
-    var url = "https://leightenore.github.io/unit-4-game/";
-    window.open(url, '_blank');
-});
-
-
-
-
-
